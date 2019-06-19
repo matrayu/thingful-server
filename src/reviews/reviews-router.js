c0fc2a72ad6dc7657606a52e19b75236e3dec48f
matrayu <northridge614@gmail.com>
2019-06-18 18:56:59 -0700

    Update reviews-router.js

--- src/reviews/reviews-router.js
+++ src/reviews/reviews-router.js
@@ -1,13 +1,14 @@
 const express = require('express')
 const path = require('path')
 const ReviewsService = require('./reviews-service')
+const { requireAuth } = require('../middleware/basic-auth')
 
 const reviewsRouter = express.Router()
 const jsonBodyParser = express.json()
 
 reviewsRouter
   .route('/')
-  .post(jsonBodyParser, (req, res, next) => {
+  .post(requireAuth, jsonBodyParser, (req, res, next) => {
     const { thing_id, rating, text } = req.body
     const newReview = { thing_id, rating, text }
 
@@ -16,6 +17,8 @@ reviewsRouter
         return res.status(400).json({
           error: `Missing '${key}' in request body`
         })
+    
+    newReview.user_id = req.user.id
 
     ReviewsService.insertReview(
       req.app.get('db'),
