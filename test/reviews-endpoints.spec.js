3d91e991c03e55f05df9bfca746563d5cc6b4852
matrayu <northridge614@gmail.com>
2019-06-18 18:57:05 -0700

    Update reviews-endpoints.spec.js

--- test/reviews-endpoints.spec.js
+++ test/reviews-endpoints.spec.js
@@ -32,8 +32,21 @@ describe('Reviews Endpoints', function() {
         testThings,
       )
     )
+    it(`responds 401 'Unauthorized request' when invalid password`, () => {
+      const userInvalidPass = { user_name: testUsers[0].user_name, password: 'wrong' }
+      return supertest(app)
+        .post('/api/reviews')
+        .set('Authorization', helpers.makeAuthHeader(userInvalidPass))
+        .expect(401, { error: 'Unauthorized request' })
+    })
 
-    it(`creates an review, responding with 201 and the new review`, function() {
+    it(`responds 401 'Missng basic token' when missing authorization token`, () => {
+      return supertest(app)
+        .post('/api/reviews')
+        .expect(401, { error: 'Missing basic token' })
+    })
+
+    it(`creates a review, responding with 201 and the new review`, function() {
       this.retries(3)
       const testThing = testThings[0]
       const testUser = testUsers[0]
@@ -41,10 +54,10 @@ describe('Reviews Endpoints', function() {
         text: 'Test new review',
         rating: 3,
         thing_id: testThing.id,
-        user_id: testUser.id,
       }
       return supertest(app)
         .post('/api/reviews')
+        .set('Authorization', helpers.makeAuthHeader(testUser))
         .send(newReview)
         .expect(201)
         .expect(res => {
@@ -68,7 +81,7 @@ describe('Reviews Endpoints', function() {
               expect(row.text).to.eql(newReview.text)
               expect(row.rating).to.eql(newReview.rating)
               expect(row.thing_id).to.eql(newReview.thing_id)
-              expect(row.user_id).to.eql(newReview.user_id)
+              expect(row.user_id).to.eql(testUser.id)
               const expectedDate = new Date().toLocaleString()
               const actualDate = new Date(row.date_created).toLocaleString()
               expect(actualDate).to.eql(expectedDate)
@@ -76,7 +89,7 @@ describe('Reviews Endpoints', function() {
         )
     })
 
-    const requiredFields = ['text', 'rating', 'user_id', 'thing_id']
+    const requiredFields = ['text', 'rating', 'thing_id']
 
     requiredFields.forEach(field => {
       const testThing = testThings[0]
@@ -84,7 +97,7 @@ describe('Reviews Endpoints', function() {
       const newReview = {
         text: 'Test new review',
         rating: 3,
-        user_id: testUser.id,
+        /* user_id: testUser.id, */
         thing_id: testThing.id,
       }
 
@@ -93,6 +106,7 @@ describe('Reviews Endpoints', function() {
 
         return supertest(app)
           .post('/api/reviews')
+          .set('Authorization', helpers.makeAuthHeader(testUser))
           .send(newReview)
           .expect(400, {
             error: `Missing '${field}' in request body`,
